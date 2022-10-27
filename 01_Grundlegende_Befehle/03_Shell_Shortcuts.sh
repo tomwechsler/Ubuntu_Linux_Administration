a95f9113c31ba20fa91194d6ff52b60e2eb43f4a
Tom Wechsler <tom.wechsler@outlook.com>
2022-10-27 08:57:28 +0200

    new files

---
+++ 01_Grundlegende_Befehle/03_Shell_Shortcuts.sh
@@ -0,0 +1,20 @@
+#Move to the start of the line
+CTRL + a
+
+#Move to the end of the line
+CTRL + e
+
+#Repeat the last command and arguments
+!!
+
+#Repeat the last arguments
+ESC + .
+
+#Move to your home directory
+cd
+
+#Move to the previous directory
+cd -
+
+#Recall previous command
+CTRL + r
---
+++ 01_Grundlegende_Befehle/04_Create_Directories.sh
@@ -0,0 +1,10 @@
+mkdir ~/dir1
+
+#-p creates the parent directories
+mkdir -p ~/dir2/dir3
+
+mkdir -p ~/ubuntu/{14,16,18,20,22}.04/{gold,current}
+
+sudo apt install -y tree
+
+tree ubuntu
