44cf3717de36802d4ce3a06311599ad36cd3b69b
Tom Wechsler <tom.wechsler@outlook.com>
2022-10-30 06:32:09 +0100

    new files

---
+++ 01_Essential_Commands/A03/05_File_Operations.sh
@@ -0,0 +1,8 @@
+#Copy requires read to the source file and write to the target directory
+cp
+
+#With mv we can move or rename files and requires both read and write to the source and target directories
+mv
+
+#The delete a file we can use rm, use -rf to delete a directory and its content
+rm
