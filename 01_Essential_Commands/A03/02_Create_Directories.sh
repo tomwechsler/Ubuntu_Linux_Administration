c729d48a9c3986dbbe9a15dc3421347134ed62bf
Tom Wechsler <tomwechsler@Air-von-Tom.fritz.box>
2022-11-02 10:37:26 +0100

    added lines

--- 01_Essential_Commands/A03/02_Create_Directories.sh
+++ 01_Essential_Commands/A03/02_Create_Directories.sh
@@ -3,8 +3,10 @@ mkdir ~/dir1
 #-p creates the parent directories
 mkdir -p ~/dir2/dir3
 
+#Create a folder structure
 mkdir -p ~/ubuntu/{14,16,18,20,22}.04/{gold,current}
 
 sudo apt install -y tree
 
+#Install the tree command
 tree ubuntu
