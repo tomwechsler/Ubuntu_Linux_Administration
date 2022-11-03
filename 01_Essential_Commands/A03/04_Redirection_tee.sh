d541b87b8fe06ad9ffda4aa6d72b22bf4adbc94c
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-03 16:31:30 +0100

    new lines

--- 01_Essential_Commands/A03/04_Redirection_tee.sh
+++ 01_Essential_Commands/A03/04_Redirection_tee.sh
@@ -7,6 +7,9 @@ ls -l /etc/hosts > file1
 #The default is redirecting STDOUT but we can use 2 to redirect error output
 ls -l /etc/Hosts 2> file2
 
+#>> append
+ls -l /etc/Hosts 2> file2
+
 ls /etc/hosts /etc/Hosts &> file3
 
 #The ipe or vertical bar takes the output of one command to the input of another
