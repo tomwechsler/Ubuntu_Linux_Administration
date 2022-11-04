aaa5e4289a6901576a6def3bf4213b210f8d89e1
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-04 13:25:50 +0100

    new lines

--- 01_Essential_Commands/A03/05_File_Operations.sh
+++ 01_Essential_Commands/A03/05_File_Operations.sh
@@ -1,12 +1,48 @@
 #Copy requires read to the source file and write to the target directory
 cp
 
+cp /etc/hosts ~
+
+cp -i /etc/hosts ~
+
+ls
+
+#Overrides the existing file
+cp /etc/hosts ~
+
+cp --backup=numbered /etc/hosts ~
+
+ls
+
+cp --backup=numbered /etc/hosts ~
+
+ls
+
+ctrl +l
+
+ls -l hosts*
+
+rm hosts*
+
+ls -l hosts*
+
 #With mv we can move or rename files and requires both read and write to the source and target directories
 mv
 
-mv file1 standard
+mv file1 stdout
 mv file2 error
 mv file3 combined
 
+ls
+
+mv stdout dir1
+
+ls dir1
+
+#no permission
+mv /etc/hosts .
+
 #The delete a file we can use rm, use -rf to delete a directory and its content
 rm
+
+rm -rf ubuntu
