6fb43811fe6c1b3630f4ff1dbc366257387efbaf
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-06 14:09:20 +0100

    lines ecited

--- 01_Essential_Commands/A05/02_The_Power_of_Find.sh
+++ 01_Essential_Commands/A05/02_The_Power_of_Find.sh
@@ -17,11 +17,11 @@ find / -maxdepth 1 -type l
 #Let's edit a line in hosts
 sudo vim /etc/hosts
 
-find /etc/hosts -mmin 5 2>/dev/null
+find /etc -mmin 5 2>/dev/null
 
-find /etc/hosts -mmin +5 2>/dev/null
+find /etc -mmin +5 2>/dev/null
 
-find /etc/hosts -mmin -5 -type f 2>/dev/null
+find /etc -mmin -5 -type f 2>/dev/null
 
 The find command dynamically searches the given directory structure for files meeting the given criteria. 
 As well as printing we can run commands against each found file.
