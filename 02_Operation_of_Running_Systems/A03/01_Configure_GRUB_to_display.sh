2900e3f43af6f8837841b7715ee7423fd5ab63c6
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-09 15:13:19 +0100

    lines edited

--- 02_Operation_of_Running_Systems/A03/01_Configure_GRUB_to_display.sh
+++ 02_Operation_of_Running_Systems/A03/01_Configure_GRUB_to_display.sh
@@ -1,10 +1,21 @@
+#Display the boot menu
 sudo vim /etc/default/grub
+
+#GRUB_TIMEOUT_STYLE=hidden (replace with menu)
+#GRUB_TIMEOUT=0 (replace with 10)
 
 sudo vim /etc/default/grub.d/50-cloudimg-settings.cfg
 
+#GRUB_TIMEOUT=0 (delete the line or commented out)
+
+sudo grub-mkconfig
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 sudo update-grub
 
-
+#Watch in virtualbox
+sudo reboot
 
 We must first configure GRUB to display the menu. This is normally via the /etc/default/grub file
 but we also must check for settings being reset in /etc/default/grub.d/
