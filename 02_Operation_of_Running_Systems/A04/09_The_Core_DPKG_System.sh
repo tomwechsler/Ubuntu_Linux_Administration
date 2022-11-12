28499348a313e49be8713bb2994e6457b22c4923
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-12 14:31:37 +0100

    lines edited

--- 02_Operation_of_Running_Systems/A04/09_The_Core_DPKG_System.sh
+++ 02_Operation_of_Running_Systems/A04/09_The_Core_DPKG_System.sh
@@ -1,13 +1,17 @@
 dpkg -l
 
-dpkg -L ufw
+dpkg -L postfix
 
-dpkg -S /usr/share/ufw/after.rules
+dpkg -S /etc/xattr.conf
 
 sudo dpkg-reconfigure postfix
 
+sudo systemctl reload postfix
+
+ss -ntl
 
 
-It is the Debian Packager that is used as the basis of APT. We canuse this to list
+
+It is the Debian Packager that is used as the basis of APT. We can use this to list
 packages, package contents and locate a file to its parent package. We can also
 force installation scripts to re-execute for simple changes to package configuration.
