f79d33c56da41f09dcf59b2844d598f6d026cc85
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-14 14:35:22 +0100

    lines edited

--- 02_Operation_of_Running_Systems/A08/08_Sharing_CPU.sh
+++ 02_Operation_of_Running_Systems/A08/08_Sharing_CPU.sh
@@ -2,15 +2,15 @@ sleep 1000 &
 
 ps -lfp $(pgrep sleep)
 
-nice -n10 sleep 1000 &
+nice -10 sleep 1000 &
 
 ps -lfp $(pgrep sleep)
 
-renice -n 19 -p <PID>
+renice 19 -p <PID>
 
-renice -n 15 -p <PID>
+sudo renice -15 -p <PID>
 
-
+ps -alf
 
 The nice value of a process determines the CPU priority that the process will get,
 ranging from -20 to +19. Running processes can be reniced to nicer values by
