f8e57b542cc1e62654d3f83b43047c3dc9118ed8
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-18 16:26:55 +0100

    lines edited

--- 03_User_and_Group_Management/A04/01_User_Passwords.sh
+++ 03_User_and_Group_Management/A04/01_User_Passwords.sh
@@ -1,12 +1,24 @@
-ls -l /etc/shadow
+sudo useradd -m user8 -s /bin/bash
 
-ls -l /usr/bin/passwd
+#To set the password we -p or --password (but it is not so easy)
+sudo useradd --help
 
-sudo cat /etc/shadow
+sudo useradd -m user9 -s /bin/bash -p $(openssl passwd -6 P@ssw0rd)
 
-sudo getent shadow vagrant
+#Let's have a look at the fields
+sudo tail -n2 /etc/shadow
+
+date -d '1 Jan 1970'
+
+date -d '1 Jan 1970 19265 days'
+
+sudo chage -l user8
 
 man 5 shadow
+
+sudo getent shadow
+
+sudo getent shadow user8
 
 
 
--- 03_User_and_Group_Management/A04/02_Managing_User_Passwords.sh
+++ 03_User_and_Group_Management/A04/02_Managing_User_Passwords.sh
@@ -1,13 +1,38 @@
-sudo passwd user1
-echo 'user1:[Password123?]' | sudo chpasswd
+sudo passwd user8
+
+sudo getent shadow user8
+
+sudo passwd -l user8
+
+sudo getent shadow user8
+
+sudo passwd -u user8
+
+sudo getent shadow user8
+
+#Notice we start with a space (this line will not show in the history)
+ echo "user8:P@ssw0rd" | sudo chpasswd
+
+#Nope no entry
+history
+
+sudo getent shadow user8
+
+#Interactive mode to set the password
 sudo chpasswd
-user1:[Password1]
-user2:[Password2]
+
+user8:P@ssw0rd
+user9:P@ssw0rd
+
 CTRL+D
+
+#Copy the users from above
+vim users
+
 sudo chpasswd < users
 
 
 
-Although we are used to the passwd command, we have to enter a user`s password twice to set
+Although we are used to the passwd command, we have to enter a users password twice to set
 it. If we are creating many users, it is not the best idea. To script password management we
 have chpasswd.
