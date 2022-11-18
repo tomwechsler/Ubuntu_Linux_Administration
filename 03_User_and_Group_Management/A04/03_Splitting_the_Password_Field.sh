e87aff5cc840f05e745614b620bcb4019f486c08
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-18 16:31:54 +0100

    lines edited

--- 03_User_and_Group_Management/A04/03_Splitting_the_Password_Field.sh
+++ 03_User_and_Group_Management/A04/03_Splitting_the_Password_Field.sh
@@ -1,5 +1,14 @@
-sudo getent shadow $USER | cut -f2 -d: |\
-awk -F'$' '{ print "ALG:\t" $2 "\nSALT:\t" $3 "\nHASH:\t" $4 }'
+sudo getent shadow $USER
+
+sudo getent shadow $USER | cut -f2 -d:
+
+sudo getent shadow $USER | cut -f2 -d: | awk -F$ '{ print $2 }'
+
+sudo getent shadow $USER | cut -f2 -d: | awk -F$ '{ print $3 }'
+
+sudo getent shadow $USER | cut -f2 -d: | awk -F$ '{ print $4 }'
+
+sudo getent shadow $USER | cut -f2 -d: | awk -F$ '{ print "ALG:\t" $2 "\nSALT:\t" $3 "\nHASH:\t" $4 }'
 
 
 
