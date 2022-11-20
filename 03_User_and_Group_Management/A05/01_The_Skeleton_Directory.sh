d48051489d63f8e09828d3dfbc171d1c90ccfd87
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-20 07:21:27 +0100

    lines edited

--- 03_User_and_Group_Management/A05/01_The_Skeleton_Directory.sh
+++ 03_User_and_Group_Management/A05/01_The_Skeleton_Directory.sh
@@ -4,7 +4,21 @@ ls -A ~test_user
 
 ls -A /etc/skel
 
+cd /etc/skel
 
+#Create a new file wirh some text
+vim README
+
+cd
+
+ls -A /etc/skel
+
+#Nothing happend to the existing user home
+ls -a ~test_user
+
+sudo useradd -m test_user2
+
+ls -A ~test_user2
 
 The user home directory is created from /etc/skel by default. The content only affects new
 home directories and not existing content.
