eccaa403e118f4d313762f538af9410d3847fcf8
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-20 07:29:57 +0100

    lines edited

--- 03_User_and_Group_Management/A05/04_Login_Script_Examples.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update && sudo apt install –y bash-doc
-
-ls /usr/share/doc/bash/examples/startup-files/
-
-
-
-Installing the package bash-doc will add example login scripts.
---
+++ 03_User_and_Group_Management/A05/04_Working_with_Login_Shells_.sh
@@ -0,0 +1,45 @@
+sudo apt update && sudo apt install –y bash-doc
+
+ls /usr/share/doc/bash/examples/startup-files/
+
+cat .profile
+
+umask
+
+grep '#umask' .profile
+
+sed –i '/^#umask/s/^#//' .profile ##uncomment the umask setting
+
+grep 'umask' .profile
+
+#To check the settings use the source command or logout/login
+source .profile
+
+#Undo the settings
+sed –i '/^umask/s/^/#/' .profile ##to comment the line
+
+!g
+
+cat .bash_logout
+
+#Does not work
+/usr/bin/clear_console
+
+tty
+
+sed -i '$a clear' .bash_logout #appends to the last line
+
+#Some commands
+ls
+ls -la
+
+#Now logout
+
+#Undo the settings
+sed –i '$d' .bash_logout #deletes the last line
+
+cat .bash_logout
+
+
+
+Installing the package bash-doc will add example login scripts.
