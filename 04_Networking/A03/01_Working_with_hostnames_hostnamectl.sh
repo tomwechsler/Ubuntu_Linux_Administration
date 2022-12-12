ff696fb0077ac0517f5746aa211bef3efb53c2a6
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-12 13:57:39 +0100

    new file

---
+++ 04_Networking/A03/01_Working_with_hostnames_hostnamectl.sh
@@ -0,0 +1,57 @@
+#Lets start
+hostnamectl
+
+#Are there any "machine" file
+
+ls -l /etc/mach*
+
+#Hostname is not like hostname
+cat /etc/hostname
+
+#Print out
+hostname
+
+#Change the hostname (is not permanent)
+sudo hostname server1
+
+#Print out
+hostname
+
+#Check with hostname config file
+!ca
+
+#With hostnamectl
+hostnamectl
+
+#Set a "pretty" hostname
+sudo hostnamectl set-hostname "tom's vm"
+
+#Check hostnamectl
+hostnamectl
+
+#Check with hostname config file
+!ca
+
+#The machine info file
+cat /etc/machine-info
+
+#Set a location
+sudo hostnamectl set-location Zurich
+
+#The machine type
+sudo hostnamectl set-deployment dev
+
+#Check hostnamectl
+hostnamectl
+
+#Check the machine config file
+!ca
+
+#The machine id
+cat /etc/machine-id
+
+#Show the OS
+hostnamectl
+
+#The os-release file
+cat /etc/os-release
