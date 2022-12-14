915a07d90c5b8937cdd916e27cd2ffabed98d3fe
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-14 13:55:41 +0100

    new file

---
+++ 04_Networking/A05/03_Configuring_linux_router.sh
@@ -0,0 +1,30 @@
+#sysctl is used to modify kernel parameters at runtime
+sysctl
+
+#Lets have a look at ip_forward
+sysctl -ar ip_forward 
+
+#The same info with
+cat /proc/sys/net/ipv4/ip_forward
+
+#We change the config
+sudo vim /etc/sysctl.conf
+
+#Remove the # at net.ipv4.ip_forward
+#Save and exit
+
+#Let's check
+sudo sysctl -p
+
+#Another check
+cat /proc/sys/net/ipv4/ip_forward
+
+#Another proof
+sysctl -ar ip_forward
+
+
+
+
+To forward packets for other hosts to remote networks, those networks that you are not
+connected to, routing must be configured. We can store the setting in the /etc/sysctl.conf or
+the /etc/sysctl.d/
