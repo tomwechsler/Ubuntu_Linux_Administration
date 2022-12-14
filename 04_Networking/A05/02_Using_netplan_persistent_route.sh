12d824324f8e6c1e5d04bdf0e4079d8612058c2c
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-14 13:49:12 +0100

    new file

---
+++ 04_Networking/A05/02_Using_netplan_persistent_route.sh
@@ -0,0 +1,29 @@
+#On ubuntu1
+sudo vim /etc/netplan/50-vagrant.yaml
+
+---
+network:
+  version: 2 
+  renderer: networkd
+  ethernets:
+    enp0s8:
+      addresses:
+      - 192.168.56.101/24 
+      routes:
+      - to: 192.168.2.0/24 
+        via: 192.168.56.102 
+
+#Lets apply the config
+sudo netplan apply
+
+#Check the route table
+ip route show
+
+#Can we ping ubuntu2
+ping 192.168.2.1
+
+
+
+
+
+Adding the route to netplan on Ubuntu 1 the knowledge on the remote network is retained.
