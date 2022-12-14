6f1c6a96e6ebdab8476060402f5611492de217e1
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-14 13:12:42 +0100

    new file

---
+++ 04_Networking/A04/01_Enable_time_ntp.sh
@@ -0,0 +1,37 @@
+#We working with the systemd eco system and use the following command
+timedatectl
+
+#We set the timezone
+sudo timedatectl set-timezone 'Europe/London' 
+
+#Lets check
+timedatectl
+
+#We have a look at the service
+systemctl status systemd-timesyncd.service
+
+#We can start the service with systemctl or
+sudo timedatectl set-ntp true
+
+#Check again
+sudo timedatectl
+
+#What about the service
+systemctl status systemd-timesyncd.service
+
+#To stop
+sudo timedatectl set-ntp false
+
+#Lets proof
+sudo systemctl status systemd-timesyncd 
+
+#And start again
+sudo timedatectl set-ntp true
+
+#Lets proof again
+sudo systemctl status systemd-timesyncd 
+
+
+
+By default, Ubuntu makes use of the NTP client-only systemd-timesyncd; giving systems a
+smaller footprint and increased security. The NTP client can be managed via timedatectl.
