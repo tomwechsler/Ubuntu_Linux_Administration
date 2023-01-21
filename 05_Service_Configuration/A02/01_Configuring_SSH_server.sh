5ce9fbaa5e0d6394dd7dd08e41460ac8136f75db
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-21 06:54:13 +0100

    lines edited

--- 05_Service_Configuration/A02/01_Configuring_SSH_server.sh
+++ 05_Service_Configuration/A02/01_Configuring_SSH_server.sh
@@ -5,3 +5,26 @@ sudo useradd -m -s /bin/bash tux
 
 #Set the password
 sudo passwd tux
+
+#List the sshd config (-T test's the config and lists them)
+sudo sshd -T
+
+#Search in the config
+sudo sshd -T | grep -E '(password|permit)'
+
+#Change the root login to "no"
+sudo vim /etc/ssh/sshd_config
+
+#PermitRootLogin prohibit-password
+PermitRootLogin no
+
+#Save and exit
+
+#Check the config
+sudo sshd -T | grep -E '(password|permit)'
+
+#-t will just check thw config without an output
+sudo sshd -t
+
+#Restart the service
+sudo systemctl restart ssh
