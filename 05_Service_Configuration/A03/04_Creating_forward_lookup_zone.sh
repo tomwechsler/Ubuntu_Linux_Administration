d03cdffae4b66f10443d244cc8fed03e3e05270a
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-22 14:14:17 +0100

    new files

---
+++ 05_Service_Configuration/A03/04_Creating_forward_lookup_zone.sh
@@ -0,0 +1,45 @@
+#Working on Ubuntu2
+
+#Lets edit the config
+sudo vim /etc/bind/named.conf.local 
+
+zone "local" {
+  type primary;
+  file "/etc/bind/zones/db.local";
+};
+
+#Check the config
+sudo named-checkconf /etc/bind/named.conf.local
+
+#To store our zone files
+sudo mkdir /etc/bind/zones
+
+#List the directories
+ls /etc/bind
+
+#Lets create the forward lookup zone file (use the db.locl file)
+sudo vim /etc/bind/zones/db.local
+
+#Ceck the config
+sudo named-checkzone local /etc/bind/zones/db.local
+
+#Reload the service
+sudo rndc reload
+
+#And lets test
+ping ubuntu1.local
+ping ubuntu2.local
+dig local. NS
+dig +short local. NS
+
+
+
+
+
+
+We want to host the DNS domain "local". We will have hostname or address records for
+ubuntu1.local, ubuntu2.local nd so on. The first step is to create the zone in the configuration
+file.
+
+We can check the zone and reload. From both systems we should be able to ping ubuntu1 and
+ubuntu2 by full or shortnames. Using dig, we can query nameserver record types.
---
+++ 05_Service_Configuration/A03/05_Creating_reverse_lookup_zone.sh
@@ -0,0 +1,32 @@
+#Working on ubuntu2
+
+#Edit the config file
+sudo vim /etc/bind/named.conf.local
+
+zone "56.168.192.in-addr.arpa" {
+  type primary;
+  file "/etc/bind/zones/db.192.168.56";
+};
+
+#Save and exit
+
+#Check the config
+sudo named-checkconf /etc/bind/named.conf.local
+
+#Create the reverse zone file (use the db.192.168.56 file)
+sudo vim /etc/bind/zones/db.192.168.56
+
+#Ceck the zone file
+sudo named-checkzone 56.168.192.in-addr.arpa /etc/bind/zones/db.192.168.56
+
+#Reload the service
+sudo rndc reload
+
+#Test the zones
+host 192.168.56.101
+dig +short -x 192.168.56.102
+
+
+
+Reverse zones allows us to query names from IP addresses. A reverse zone is the 
+IP address reversed with in-addr.arpa at the and. We have PTR records in place of A records.
