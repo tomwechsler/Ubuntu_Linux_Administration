315defaca5969d0831bc2bd365d8679dae0ece83
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-26 07:04:01 +0100

    new files

--- 05_Service_Configuration/A06/04_Adding_MX_records.sh
+++ 05_Service_Configuration/A06/04_Adding_MX_records.sh
@@ -1,11 +1,19 @@
+#Working on ubuntu2
+
+#We edit our zone file
 sudo vim /etc/bind/zones/db.local 
 
-local. IN MX 10 ubuntu2.local. 
+local. IN MX 10 ubuntu2.local. #Dont forget to update the serial number of the zone
 
+#Save and exit
+
+#Check the zone file
 sudo named-checkzone local. /etc/bind/zones/db.local 
 
+#Reload the service
 sudo rndc reload local. IN 
 
+#Did it work?
 dig local. MX 
 
 
