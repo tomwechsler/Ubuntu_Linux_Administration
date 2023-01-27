82c939aa7f34e4b27380a32d4c66fd0e747bfaa3
Tom Wechsler <tom.wechsler@outlook.com>
2023-01-27 07:07:48 +0100

    new files

--- 05_Service_Configuration/A08/03_vagrant_libvirt.sh
+++ 05_Service_Configuration/A08/03_vagrant_libvirt.sh
@@ -1,15 +1,33 @@
+#Working on ubuntu2
+
+#Install vagrant
 sudo apt -y vagrant
 
+#Create a direcrory and move in
 mkdir test && cd test 
 
+#Create the Vagrantfile
 vagrant init --minimal generic/ubuntu2004
 
+#Show the Vagrantfile
+cat Vagrantfile
+
+#Start the vm with the libvirt provider
 vagrant up --provider=libvirt
 
-virsh list
-
+#Connect to the vm
 vagrant ssh
 
+exit
+
+#List the vm
+virsh list
+
+#Stop the vm
+vagrant halt
+
+#List the vm
+virsh list --all
 
 
 As we have become used to Vagrant in this course so far, we can use it in our Ubuntu system to
