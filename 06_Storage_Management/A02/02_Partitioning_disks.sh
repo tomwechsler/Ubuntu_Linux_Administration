e9417e44ae0686d30dd173f3f0b4bd30af81123a
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-23 06:55:20 +0100

    new files

--- 06_Storage_Management/A02/02_Partitioning_disks.sh
+++ 06_Storage_Management/A02/02_Partitioning_disks.sh
@@ -1,10 +1,33 @@
 #MSDOS = 4 Primary max. Size 2TB
 #GPT = theoretical 128 Partitions (SCSI Driver max 15 Partitions) max. Size 32EB
 
+#Working on ubuntu1
 
+#List the block device
+lsblk
 
+#Partition with fdisk
+sudo fdisk /dev/loop3
 
-sudo parted /dev/loop3 mklabel msdos mkpart primary 0% 100% print 
+#Now we have interactive utility => We create 1 Partition with the full size
+
+#Does lsblk list the partition
+lsblk
+
+#We need partprobe
+sudo partprobe /dev/loop3
+
+#Now it does
+lsblk
+
+#Lets wipe
+sudo wipefs -a /dev/loop3 # -a = all
+
+#Partition with parted
+sudo parted /dev/loop3 mklabel msdos mkpart primary 0% 50% mkpart primary 50% 100% print 
+
+#Did it work
+lsblk
 
 
 
