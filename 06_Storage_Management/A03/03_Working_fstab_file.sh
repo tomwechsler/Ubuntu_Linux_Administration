f0f06a23e8ef03510ee965b0a1d13745022b80c1
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-26 06:57:05 +0100

    new files

--- 06_Storage_Management/A03/03_Working_fstab_file.sh
+++ 06_Storage_Management/A03/03_Working_fstab_file.sh
@@ -1,14 +1,42 @@
 #Working on ubuntu1
 
-sudo blkid /dev/loop3p1 /dev/loop3p2
+#Check the mounts
+mount -t xfs
 
+mount -t ext4
+
+#List the block devices
+lsblk -f
+
+#Copy the UUID from loop3p1
+
+#Edit the fstab file
 sudo vim /etc/fstab
 
+UUID="the uuid" /shared_ext4 ext4 defaults 0 0
+
+:r!sudo blkid /dev/loop3p2 #Import the infos
+
+#Edit the line
+
+#Save and exit
+
+#Umount
 sudo unmount /shared_ext4
 
 sudo unmount /shared_xfs
 
+#But what about the fstab fields
+man fstab
+
+#List the block devices
+lsblk -f
+
+#Use the mount command
 sudo mount -a
+
+#List the block devices
+lsblk -f
 
 
 
