197c0559e78c1cf60ef45a02b7c52a04e6d4318b
Tom Wechsler <tom.wechsler@outlook.com>
2023-02-28 07:00:23 +0100

    new files

---
+++ 06_Storage_Management/A04/02_Tuning_swap_behaviour.sh
@@ -0,0 +1,32 @@
+#Working on ubuntu1 in a root session
+
+#The swappiness range goes from 0 to 100 (default is 60). 0 = No swapping; 100 = Aggressive Swapping
+
+sudo -i
+
+#Check the swappiness
+cat /proc/sys/vm/swappiness
+
+#Is there a config in place
+grep -R swap /etc/sysctl*
+
+#Create a file
+vim /etc/sysctl.d/99-swap.conf
+
+vm.swappiness=20
+
+#Save and exit
+
+#Read in all config files
+sysctl --system
+
+#Check
+!g
+
+!c
+#or
+cat /proc/sys/vm/swappiness 
+
+
+
+We can tune how much the kernel is likely to swap using sysctl.
